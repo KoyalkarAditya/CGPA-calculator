866db58d1afe574b96796f3558700b764df59c69
KoyalkarAditya <koyalkaraditya123@gmail.com>
2023-11-27 09:13:46 +0530

    modified script

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 let count = 1;
 let content = document.querySelector(".outer-container");
 document.querySelector(".addSubject").addEventListener("click", () => {
-  count += 1;
-  addSubject(count);
+  addSubject();
 });
-function addSubject(count) {
-  let addSubjectHtml = `
+let addSubjectHtml = `
 <div class="outer-container">
             <div class="container">
               <div class="subject-no">Subject: ${count}</div>
@@ -22,25 +20,28 @@ function addSubject(count) {
                 />
               </div>
             </div>`;
-  content.innerHTML += addSubjectHtml;
+function addSubject() {
+  count += 1;
+  let newSubjectHtml = `
+    <div class="container">
+      <div class="subject-no">Subject: ${count}</div>
+      <div class="grade-credits">
+        <input
+          type="number"
+          id="grade"
+          placeholder="Enter grade points"
+        />
+        <input
+          type="number"
+          id="credits"
+          placeholder="Enter credit points"
+        />
+      </div>
+    </div>`;
+
+  content.innerHTML += newSubjectHtml;
 }
 document.querySelector(".reset").addEventListener("click", () => {
   count = 1;
-  content.innerHTML = `
-<div class="outer-container">
-            <div class="container">
-              <div class="subject-no">Subject: ${count}</div>
-              <div class="grade-credits">
-                <input
-                  type="number"
-                  id="grade"
-                  placeholder="Enter grade points"
-                />
-                <input
-                  type="number"
-                  id="credits"
-                  placeholder="Enter credit points"
-                />
-              </div>
-            </div>`;
+  content.innerHTML = addSubjectHtml;
 });
