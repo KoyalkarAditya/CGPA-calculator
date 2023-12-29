d3a9551b2186efb155a963f640018f9bf67724d8
KoyalkarAditya <koyalkaraditya123@gmail.com>
2023-12-29 22:34:50 +0530

    final-script added

--- script.js
+++ script.js
@@ -1,47 +1,95 @@
-let count = 1;
-let content = document.querySelector(".outer-container");
-document.querySelector(".addSubject").addEventListener("click", () => {
-  addSubject();
-});
-let addSubjectHtml = `
-<div class="outer-container">
-            <div class="container">
-              <div class="subject-no">Subject: ${count}</div>
+let subjectNumber = 1;
+let subjectsContainer = document.querySelector(".container");
+let addSubjectButton = document.querySelector(".addSubject");
+let calculateBtn = document.querySelector(".calculate");
+let resetBtn = document.querySelector(".reset");
+let gradeInputs = subjectsContainer.querySelectorAll(".grade");
+let creditInputs = subjectsContainer.querySelectorAll(".credits");
+let grandeCreditsDiv = document.querySelectorAll(".grade-credits");
+let inputValues = [];
+let subjectHTML = `
+<div>
+<div class="subject-no">Subject: ${subjectNumber}</div>
               <div class="grade-credits">
                 <input
+                  class="grade"
                   type="number"
                   id="grade"
                   placeholder="Enter grade points"
                 />
                 <input
+                  class="credits"
                   type="number"
                   id="credits"
                   placeholder="Enter credit points"
                 />
               </div>
-            </div>`;
-function addSubject() {
-  count += 1;
-  let newSubjectHtml = `
-    <div class="container">
-      <div class="subject-no">Subject: ${count}</div>
-      <div class="grade-credits">
-        <input
-          type="number"
-          id="grade"
-          placeholder="Enter grade points"
-        />
-        <input
-          type="number"
-          id="credits"
-          placeholder="Enter credit points"
-        />
-      </div>
-    </div>`;
-
-  content.innerHTML += newSubjectHtml;
-}
-document.querySelector(".reset").addEventListener("click", () => {
-  count = 1;
-  content.innerHTML = addSubjectHtml;
+            </div>
+            </div>
+`;
+addSubjectButton.addEventListener("click", () => {
+  addSubject();
 });
+resetBtn.addEventListener("click", () => {
+  reset();
+});
+calculateBtn.addEventListener("click", () => {
+  calculateSGPA();
+});
+function addSubject() {
+  subjectNumber++;
+  let newSubject = document.createElement("div");
+  newSubject.innerHTML = `
+<div>
+<div class="subject-no">Subject: ${subjectNumber}</div>
+              <div class="grade-credits">
+                <input
+                  class="grade"
+                  type="number"
+                  id="grade"
+                  placeholder="Enter grade points"
+                />
+                <input
+                  class="credits"
+                  type="number"
+                  id="credits"
+                  placeholder="Enter credit points"
+                />
+              </div>
+            </div>
+            </div>
+`;
+  subjectsContainer.appendChild(newSubject);
+}
+function reset() {
+  subjectNumber = 1;
+  subjectsContainer.innerHTML = subjectHTML;
+  inputValues = [];
+  document.querySelector(".output").innerHTML = ``;
+}
+
+function calculateSGPA() {
+  inputValues = [];
+  let gradeInputs = subjectsContainer.querySelectorAll(".grade");
+  let creditInputs = subjectsContainer.querySelectorAll(".credits");
+
+  gradeInputs.forEach((gradeInput, index) => {
+    let grade = parseFloat(gradeInput.value);
+    let credit = parseFloat(creditInputs[index].value);
+    if (!isNaN(grade) && !isNaN(credit)) {
+      inputValues.push({ grades: grade, credits: credit });
+    }
+  });
+  let totalScore = 0,
+    totalCredits = 0;
+  for (let i = 0; i < inputValues.length; i++) {
+    if (inputValues[i].grades && inputValues[i].credits) {
+      totalScore += inputValues[i].grades * inputValues[i].credits;
+      totalCredits += inputValues[i].credits;
+    }
+  }
+  if (totalCredits !== 0) {
+    let CGPA = totalScore / totalCredits;
+    document.querySelector(".output").innerHTML = `CGPA=${CGPA.toFixed(2)}`;
+  }
+}
